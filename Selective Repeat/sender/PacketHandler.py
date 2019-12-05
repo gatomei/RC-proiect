f26a658f4b57c2100e817409397f22daebedd9ba
gatomei <atomeigeorgiana@yahoo.com>
2019-12-06 00:36:44 +0200

    sender_side-transmission_works
    Signed-off-by: gatomei <atomeigeorgiana@yahoo.com>

---
+++ Selective Repeat/sender/PacketHandler.py
@@ -0,0 +1,40 @@
+from threading import Thread
+import time
+from threading import Lock
+lock = Lock()
+
+class PacketHandler(Thread):
+    def __init__(self, senderSocket, receiverIP, receiverPort, packet, timeout, window, logger, threadName="Packet(?)"):
+        Thread.__init__(self)
+        self.senderSocket = senderSocket
+        self.receiverIP = receiverIP
+        self.receiverPort = receiverPort
+        self.packet = packet
+        self.timeout = timeout
+        self.window = window
+        self.logger = logger
+        self.threadName = threadName
+
+    def run(self):
+        self.logger.info(f"On Thread {self.threadName }-Sending packet no {self.packet.sequenceNo} \n")
+        self.send()
+        while self.window.unacked(int(self.packet.sequenceNo)) and \
+                self.window.getStartTime(int(self.packet.sequenceNo)):
+            timpScurs = time.time() - self.window.getStartTime(int(self.packet.sequenceNo))
+            if timpScurs > self.timeout:
+                self.logger.info(f"Packet no {self.packet.sequenceNo} lost.Retransmitting it\n")
+                self.send()
+                self.window.restartTimer(int(self.packet.sequenceNo))
+            self.window.mark_ack(int(self.packet.sequenceNo))
+
+        self.window.stopTimer(int(self.packet.sequenceNo))
+        self.window.ackRecv(int(self.packet.sequenceNo))
+
+
+
+    def send(self):
+        pkt = self.packet.pack()
+        self.senderSocket.sendto(bytes(pkt, 'utf-8'), (self.receiverIP, self.receiverPort))
+        self.window.startTimer(int(self.packet.sequenceNo))
+
+
