c0e9af2768d39177b9cf22f44e21941b7b4631ed
gatomei <atomeigeorgiana@yahoo.com>
2020-01-16 16:22:30 +0200

    final status

--- Selective Repeat/sender/PacketHandler.py
+++ Selective Repeat/sender/PacketHandler.py
@@ -14,6 +14,8 @@ class PacketHandler(Thread):
         self.window = window
         self.logger = logger
         self.threadName = threadName
+        self.resendNo = 0
+        self.maxResendNo = 3
 
     def run(self):
         self.logger.info(f"On Thread {self.threadName }-Sending packet no {self.packet.sequenceNo} ")
@@ -22,18 +24,26 @@ class PacketHandler(Thread):
                 self.window.getStartTime(int(self.packet.sequenceNo)):
             timpScurs = time.time() - self.window.getStartTime(int(self.packet.sequenceNo))
             if timpScurs > self.timeout:
-                self.logger.info(f"Packet no {self.packet.sequenceNo} lost.Retransmitting it")
-                self.send()
-                self.window.restartTimer(int(self.packet.sequenceNo))
-            self.window.mark_ack(int(self.packet.sequenceNo))
+                if self.resendNo < self.maxResendNo:
+                    self.resend()
+                    self.resendNo += 1
+                else:
+                    self.window.mark_ack(int(self.packet.sequenceNo))
+                    self.logger.error(f"On Thread {self.threadName }-Abandoned resending for packet no  {self.packet.sequenceNo} ")
+                    break
 
-        self.window.stopTimer(int(self.packet.sequenceNo))
+
+
         self.window.ackRecv(int(self.packet.sequenceNo))
-
-
 
     def send(self):
         pkt = self.packet.pack()
         self.senderSocket.sendto(bytes(pkt, 'utf-8'), (self.receiverIP, self.receiverPort))
         self.window.startTimer(int(self.packet.sequenceNo))
 
+    def resend(self):
+        self.packet.sequenceNo = str(int(self.packet.sequenceNo))
+        pkt = self.packet.pack()
+        self.logger.info(f"Packet no {int(self.packet.sequenceNo)} lost.Retransmitting it")
+        self.senderSocket.sendto(bytes(pkt, 'utf-8'), (self.receiverIP, self.receiverPort))
+        self.window.restartTimer(int(self.packet.sequenceNo))
